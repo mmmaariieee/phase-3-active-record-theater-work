ba771b3c3b5db3827f9fbd38b08298a2e882c353
Mariia <m.goncharenkoo@icloud.com>
2022-11-06 23:25:17 -0500

    commit tree 8c4f7df4d4a9420860a969b6ef030b374533a4e5

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Role.create(character_name: "A")
+Role.create(character_name: "B")
+Role.create(character_name: "C")
+
+Audition.create(actor: "Emilia", location: "New York", phone: 6563879797, hired: true, role_id: 1)
+Audition.create(actor: "John", location: "San Diego", phone: 4568274634, hired: false, role_id: 2)
+Audition.create(actor: "Patrick", location: "DC", phone: 9548362958, hired: true, role_id: 1)
+Audition.create(actor: "Melanie", location: "Las Vegas", phone: 8573629365, hired: false, role_id: 3)
+Audition.create(actor: "Kemit", location: "Detroit", phone: 4837264856, hired: true, role_id: 2)
